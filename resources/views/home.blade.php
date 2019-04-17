1441fb02dd48f84f55293031ebbee38bc31d7e32
RukkiesMan <pavelekseev@gmail.com>
2019-04-17 20:33:37 +0300

    Make form workable

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,26 +16,26 @@
                     <form action="{{ route('exchange.post') }}" method="POST">
                         <div class="form-group">
                             <label for="exchanged_currency">Откуда списываем ваши денюжки ?</label>
-                            <select class="form-control" id="exchanged_currency">
+                            <select class="form-control" id="exchanged_currency" name="exchanged_cur">
                                 <option> Православный счет(Рубль   500)</option>
                             </select>
                         </div>
 
                         <div class="form-group">
                             <label for="received_currency">Какой кошель пополняем, Господин ?</label>
-                            <select class="form-control" id="received_currency">
+                            <select class="form-control" id="received_currency" name="received_cur">
                                 <option>Американцев поддерживаю (доллары)</option>
                             </select>
                         </div>
 
                         <div class="form-group">
                             <label for="received_currency">Курсик выбранной вами валюты: </label>
-                            <input type="text" class="form-control" value="65" disabled>
+                            <input type="text" class="form-control" name="rate" value="65" disabled>
                         </div>
 
                         <div class="form-group">
                             <label for="amount">Сколько башляем ?</label>
-                            <input type="text" class="form-control">
+                            <input type="text" class="form-control" name="amount">
                         </div>
 
                         <div class="form-group">
